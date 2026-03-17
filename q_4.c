e053530087a0a6c422a0c1dbf8247eea9348e404
bhoomisadhvani <bhoomisadhvani@gmail.com>
2026-03-17 12:30:49 +0530

    Add files via upload

---
+++ q_4.c
@@ -0,0 +1,52 @@
+#include<stdio.h>
+
+int main()
+{
+    int r,c,i,j;
+    int sum=0;
+
+    printf("Enter row size: ");
+    scanf("%d",&r);
+
+    printf("Enter column size: ");
+    scanf("%d",&c);
+
+    int a[r][c];
+
+    printf("Enter array elements on index no.:\n");
+
+    for(i=0;i<r;i++)
+    {
+        for(j=0;j<c;j++)
+        {
+            printf("a[%d][%d] = ", i, j);
+            scanf("%d",&a[i][j]);
+        }
+    }
+
+    printf("\nAddition of rows:\n");
+
+    for(i=0;i<r;i++)
+    {
+        sum=0;
+        for(j=0;j<c;j++)
+        {
+            sum = sum + a[i][j];
+        }
+        printf("Row %d sum = %d\n", i+1, sum);  // ✅ inside loop
+    }
+
+    printf("\nAddition of columns:\n");
+
+    for(j=0;j<c;j++)
+    {
+        sum=0;
+        for(i=0;i<r;i++)
+        {
+            sum = sum + a[i][j];
+        }
+        printf("Column %d sum = %d\n", j+1, sum); // ✅ inside loop
+    }
+
+    return 0;
+}
